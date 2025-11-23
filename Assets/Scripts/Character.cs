8eb9a8e31749bdf3404af79a1e50ee62c62499ea
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2025-11-23 18:41:24 +0700

    Character scripts

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,7 +1,45 @@
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
+    private float maxHealth;
+    private float health;
+    public float Health
+    {
+        get => health;
+        set => health = (value<0) ? 0:value;
+    }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public void Tnti(int hp)
+    {
+        maxHealth = hp;
+        Health = hp;
+
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+    }
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log($"{this.name} is dead and destroyed");
+            return true;
+        }
+        else return false;
+    }
+
+    public abstract void Shoot();
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
